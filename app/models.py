ba2ae833e81cc919bb159da69d00e0df669d3788
rnantume <nnrobin37@gmail.com>
2018-08-21 14:09:42 +0300

    contains model for getting all questions

---
+++ app/models.py
@@ -0,0 +1,26 @@
+import uuid
+import datetime
+
+questions = []
+
+class Question():
+    """ 
+    class to represent question model
+    """
+    def __init__(self, Topic, Description):
+        self.questionId = uuid.uuid4().hex
+        self.Topic = Topic
+        self.Description = Description
+        self.datetimeCreated = datetime.datetime.now()
+        self.answers = []
+
+
+    @staticmethod
+    def get_questions():
+        """
+        :return: all questions in memory
+        """
+        if questions:
+            return questions 
+     
+    
