ce534db12140dcf0e4ec516a80560c4d628d2e53
rnantume <nnrobin37@gmail.com>
2018-08-21 16:52:06 +0300

    added answer model

--- app/models.py
+++ app/models.py
@@ -49,4 +49,33 @@ class Question():
         questions.append(new_question)
         return new_question
      
-    
+class Answer():
+    """
+    class to represent answer model
+    """
+
+    def __init__(self, answer, accepted):
+        self.answerId = uuid.uuid4().hex
+        self.answer = answer
+        self.accepted = accepted
+        self.datetimeCreated = datetime.datetime.now()
+        self.comments = []
+
+    def add_answer(self, questionId):
+        """
+        method to add a new answer to a question
+        :param questionId: of question to add answers
+        :return: answer created(dict) or question is none
+        """
+        for question in questions:
+            if question['questionId']== questionId:
+                new_answer = {
+                    'answerId': self.answerId,
+                    'answer': self.answer,
+                    'accepted': self.accepted,
+                    'datetimeCreated': self.datetimeCreated,
+                    'comments': self.comments
+                }
+                question['answers'].append(new_answer)
+                return new_answer
+        
