5d98152bed0fc5d78f46ef6c700024515750b36b
rnantume <nnrobin37@gmail.com>
2018-08-23 15:42:02 +0300

    added model for adding new question

--- API/models.py
+++ API/models.py
@@ -22,5 +22,19 @@ class Question():
         """
         if questions:
             return questions 
-     
+
+    def add_question(self):
+        """
+        method to add a new question
+        :return: question created(dict)
+        """
+        new_question = {
+            'questionId': self.questionId,
+            'Topic': self.Topic,
+            'Description': self.Description,
+            'datetimeCreated': self.datetimeCreated,
+            'answers': self.answers
+        }
+        questions.append(new_question)
+        return new_question
     
