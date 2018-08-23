9c4f15dd102e274350db363aca689a1fb5c5ef24
rnantume <nnrobin37@gmail.com>
2018-08-23 11:38:36 +0300

    file contains unittests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,25 @@
 import unittest
 import json
 
-from app import create_app
+from stackapi.app import create_app
 
 class QuestionsTestCase(unittest.TestCase):
     """class to represent questions test case"""
 
-    def test_create_app(self):
-        app = create_app(config_name="testing")
-
-
     def setUp(self):
-        self.app = create_app(config_name="testing")
+        self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client()
         
+    def test_add_question(self):
+        """Test API can add new question"""
+        res = self.client.post('/StackOverflow-lite/api/v1/questions', 
+                    data=json.dumps({"Topic":"Sample Topic", "Description":"Sample Description"}),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('Sample Description', str(res.data))
+
     def test_api_can_get_all_questions(self):
         """Test API can get all questions (GET request)."""
         res = self.client.post('/StackOverflow-lite/api/v1/questions',
@@ -25,14 +29,6 @@ class QuestionsTestCase(unittest.TestCase):
         res = self.client.get('/StackOverflow-lite/api/v1/questions')
         self.assertEqual(res.status_code, 200)
         self.assertIn('Description', str(res.data))
-
-    def test_add_question(self):
-        """Test API can add new question"""
-        res = self.client.post('/StackOverflow-lite/api/v1/questions', 
-                    data=json.dumps({"Topic":"Sample Topic", "Description":"Sample Description"}),
-                                 content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('Sample Description', str(res.data))
     
     def test_api_can_get_question_by_id(self):
         """Test API can get a question by using it's questionId."""
@@ -54,7 +50,7 @@ class QuestionsTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         result_in_json = json.loads(res.data.decode('utf-8').replace("'", "\""))
         result = self.client.post('/questions/{}/answers'.format(result_in_json['questionId']),
-                                    data=json.dumps({'answer':"Sample Answer", 'accepted':True}),
+                                    data=json.dumps({'answer':"Sample Answer"}),
                                     content_type='application/json')
         self.assertEqual(result.status_code, 201)
         self.assertIn('Sample Answer', str(result.data))
@@ -68,7 +64,7 @@ class QuestionsTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         result_in_json = json.loads(res.data.decode('utf-8').replace("'", "\""))
         result = self.client.post('/questions/{}/answers'.format(result_in_json['questionId']),
-                                    data=json.dumps({'answer':"Sample Answer", 'accepted':True}),
+                                    data=json.dumps({"answer":"Sample Answer"}),
                                     content_type='application/json')
         self.assertEqual(result.status_code, 201)
         self.assertIn('Sample Answer', str(result.data))
@@ -77,7 +73,7 @@ class QuestionsTestCase(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
 
     def tearDown(self):
-        pass
+    #     pass
         
 
 if __name__=='__main__':
