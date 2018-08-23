69365fe5bb4c7fbbd60dc3e7648bf18cd012bafc
rnantume <nnrobin37@gmail.com>
2018-08-23 11:36:00 +0300

    contains app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
-from instance.config import app_config
+import models
+from resources.questions import questions_bp
+from resources.answers import answers_bp
 
+# from instance.config import app_config
 
-def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-
-    
+app = Flask(__name__)
+# app.config.from_object(app_config[config_name])
+# app.config.from_pyfile('config.py')
 
     """
     registering the questions_bp and answers_bp blueprints
     """
-    from .questions import questions_bp
     app.register_blueprint(questions_bp, url_prefix='/StackOverflow-lite/api/v1')
-
-    from .answers import answers_bp
     app.register_blueprint(answers_bp, url_prefix='/StackOverflow-lite/api/v1')
-
-
-    return app
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-import os
-
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS') 
-app = create_app(config_name)
+# config_name = os.getenv('APP_SETTINGS') 
+# app = create_app(config_name)
 
+app = create_app()
 if __name__ == '__main__':
     app.run()
